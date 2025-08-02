ddd6da240767a2b8e13dbebdcba66cc504e05111
oseitutunelson <oseitutunelson11@gmail.com>
2025-08-02 02:36:40 +0000

    react

--- apicki/src/components/Meals/dummy-meals.js
+++ apicki/src/components/Meals/dummy-meals.js
@@ -2,22 +2,22 @@ const DUMMY_MEALS = [
     {
         id: "m1",
         name: "Cake Bread",
-        description: "Delight in every bite with our rich, chocolate-infused Cake Bread – a perfect treat for your sweet cravings!",
-        price: 15.50,
+        description: "Delight in every bite with our rich, date-infused Cake Bread – a perfect treat for your sweet cravings!",
+        price: 15.00,
         available: true,
     },
     {
         id: "m2",
-        name: "Butter Bread",
-        description: "Experience the authentic taste of Ghana with our soft and buttery Butter Bread, baked fresh daily just for you!",
-        price: 13.50,
+        name: "Wheat Bread",
+        description: "Experience the authentic taste of Ghana with our soft and sweat wheat Bread, baked fresh daily just for you!",
+        price: 15.00,
         available: true,
     },
     {
         id: "m3",
         name: "Coconut Bread",
         description: "Savor the tropical goodness of our Coconut Bread, bursting with fresh coconut flavor that’s simply unforgettable.",
-        price: 18.50,
+        price: 15.00,
         available: true,
     },
     // {
